3d3145a3577659a167bd3ba8c224693262ec2a8a
Víctor Navarro Aránguiz <victornavarrokp47@gmail.com>
2019-07-20 19:27:35 -0400

    initialization with nndsvd

--- tsnmf.py
+++ tsnmf.py
@@ -46,3 +46,126 @@ class TSNMF:
         self.verbose = verbose
 
 
+    def fit_transform(self, X, y=None, W=None, H=None):
+        return
+    
+def topic_supervised_factorization(X, W=None, H=None, n_components=None,
+                                    init=None, update_H=True, tol=1e-4,
+                                    max_iter=200, regularization=None,
+                                    random_state=None, verbose=0):
+    r"""
+        update_H : boolean, default: True
+            Set to True, both W and H will be estimated from initial guesses.
+            Set to False, only W will be estimated.
+
+        rgularization : 'both' | 'components' | 'transformation' | None
+            Select whether the regularization affects the components (H), the
+            transformation (W), both or none of them.
+    """
+
+    #X = check_array(X, accept_sparse=('csr','csc'), dtype=float) # from utils
+    #check_non_negative(X, "TSNMF (input X)") # from utils.validation
+    
+    n_samples, n_features = X.shape
+    if n_components is None:
+        n_components = n_features
+
+    #Validation from NMF sklearn source code
+    if not isinstance(n_components, INTEGER_TYPES) or n_components <= 0:
+        raise ValueError("Number of components must be a positive integer;"
+                         " got (n_components=%r)" % n_components)
+    if not isinstance(max_iter, INTEGER_TYPES) or max_iter < 0:
+        raise ValueError("Maximum number of iterations must be a positive "
+                         "integer; got (max_iter=%r)" % max_iter)
+    if not isinstance(tol, numbers.Number) or tol < 0:
+        raise ValueError("Tolerance for stopping criteria must be "
+                         "positive; got (tol=%r)" % tol)
+
+    W, H = _initialize_tsnmf(X, n_components, init=init, random_state=random_state)
+
+
+def _initialize_tsnmf(X, n_components, init=None, eps=1e-6, random_state=None):
+    r"""
+    Initialization of matrices W and H (X = WH)
+
+    Returns
+    -------
+    W : array-like, shape (n_samples, n_components)
+        Initial guesses for solving X ~= WH
+    H : array-like, shape (n_components, n_features)
+        Initial guesses for solving X ~= WH
+    """
+
+    #check_non_negative(X, "TSNMF initialization") # from utils.validation
+    n_samples, n_features = X.shape
+    if (init is not None and init != 'random'
+            and n_components > min(n_samples, n_features)):
+            raise ValueError("init = '{}' can only be used when "
+                         "n_components <= min(n_samples, n_features)"
+                         .format(init))
+    
+    if init is None:
+        if n_components <= min(n_samples, n_features):
+            init = 'nndsvd'
+        else:
+            init = 'random'
+   
+    if init == 'random':
+        avg = np.sqrt(X.mean() / n_components)
+        rng = check_random_state(random_state) # from utils
+        H = avg * rng.randn(n_components, n_features)
+        W = avg * rng.randn(n_samples, n_components)
+        # we do not write np.abs(H, out=H) to stay compatible with
+        # numpy 1.5 and earlier where the 'out' keyword is not
+        # supported as a kwarg on ufuncs
+        np.abs(H, H)
+        np.abs(W, W)
+        return W, H
+
+    # NNDSVD initialization
+    U, S, V = randomized_svd(X, n_components, random_state=random_state) # from utils.extmath
+    W, H = np.zeros(U.shape), np.zeros(V.shape)
+
+    # The leading singular triplet is non-negative
+    # so it can be used as is for initialization.
+    W[:, 0] = np.sqrt(S[0]) * np.abs(U[:, 0])
+    H[0, :] = np.sqrt(S[0]) * np.abs(V[0, :])
+
+    for j in range(1, n_components):
+        x, y = U[:, j], V[j, :]
+
+        # extract positive and negative parts of column vectors
+        x_p, y_p = np.maximum(x, 0), np.maximum(y, 0)
+        x_n, y_n = np.abs(np.minimum(x, 0)), np.abs(np.minimum(y, 0))
+
+        # and their norms
+        x_p_nrm, y_p_nrm = norm(x_p), norm(y_p)
+        x_n_nrm, y_n_nrm = norm(x_n), norm(y_n)
+
+        m_p, m_n = x_p_nrm * y_p_nrm, x_n_nrm * y_n_nrm
+
+        # choose update
+        if m_p > m_n:
+            u = x_p / x_p_nrm
+            v = y_p / y_p_nrm
+            sigma = m_p
+        else:
+            u = x_n / x_n_nrm
+            v = y_n / y_n_nrm
+            sigma = m_n
+
+        lbd = np.sqrt(S[j] * sigma)
+        W[:, j] = lbd * u
+        H[j, :] = lbd * v
+
+    W[W < eps] = 0
+    H[H < eps] = 0
+
+    if init == "nndsvd":
+        pass
+    else:
+        raise ValueError(
+            'Invalid init parameter: got %r instead of one of %r' %
+            (init, (None, 'random', 'nndsvd',)))
+
+    return W, H
